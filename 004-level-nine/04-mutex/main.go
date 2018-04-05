4f3832655a34ecc291ee1e9b3fbaf20b1e773443
Frank Larios <frankandrewlarios@gmail.com>
2018-04-05 12:23:15 -0700

    Adds example code of Mutex fixing a race condition

---
+++ 004-level-nine/04-mutex/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	incrementor := 0
+	gs := 100
+	wg.Add(gs)
+	var m sync.Mutex
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			m.Lock()
+			v := incrementor
+			v++
+			incrementor = v
+			fmt.Println(incrementor)
+			m.Unlock()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("end value: ", incrementor)
+}
